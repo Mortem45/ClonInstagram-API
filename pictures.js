d1c427eb6e240b50348f778f332a98bcb80150bc
Mortem45 <ste45_007@hotmail.com>
2018-11-19 09:50:08 -0600

    creando GET /:id

--- pictures.js
+++ pictures.js
@@ -2,11 +2,24 @@
 
 import { send } from 'micro'
 import HttpHash from 'http-hash'
+import Db from 'cloninstagram-db'
+import DbStub from './test/stub/db'
+import config from './config'
+
+const env = process.env.NODE_ENV || 'production'
+let db = new Db(config.db)
+if (env === 'test') {
+  db = new DbStub()
+}
 
 const hash = HttpHash()
 
-hash.set('GET /:id', async function getPicture (req, res, parms) {
-  send(res, 200, parms)
+hash.set('GET /:id', async function getPicture (req, res, params) {
+  let id = params.id
+  await db.connect()
+  let image = await db.getImage(id)
+  await db.disconnect()
+  send(res, 200, image)
 })
 
 export default async function main (req, res) {
@@ -15,7 +28,7 @@ export default async function main (req, res) {
 
   if (match.handler) {
     try {
-      await match.handler(req, res, match.parms)
+      await match.handler(req, res, match.params)
     } catch (e) {
       send(res, 500, { error: e.message })
     }
---
+++ test/fixtures/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+export default {
+  getImage () {
+    return {
+      id: 'a2322728-25ba-4ec8-b7a5-78dd7fd5e164',
+      publicId: '78dd7fd5e164',
+      userId: 'clonInstagram',
+      src: 'http://cloninstagram.test/78dd7fd5e164.jpg',
+      description: '#awesome',
+      tags: [ 'awesome' ],
+      createdAt: new Date().toString()
+    }
+  }
+}
