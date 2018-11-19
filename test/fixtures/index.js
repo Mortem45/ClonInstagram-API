310e878ec47182f8e99e564ddc99defba54c6293
Mortem45 <ste45_007@hotmail.com>
2018-11-19 11:47:01 -0600

    implementando  POST /picture, POST /picture:id/like y Get /picture/list

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -6,10 +6,20 @@ export default {
       id: 'a2322728-25ba-4ec8-b7a5-78dd7fd5e164',
       publicId: '78dd7fd5e164',
       userId: 'clonInstagram',
+      liked: false,
+      likes: 0,
       src: 'http://cloninstagram.test/78dd7fd5e164.jpg',
       description: '#awesome',
       tags: [ 'awesome' ],
       createdAt: new Date().toString()
     }
+  },
+
+  getImages () {
+    return [
+      this.getImage(),
+      this.getImage(),
+      this.getImage()
+    ]
   }
 }
--- test/pictures-test.js
+++ test/pictures-test.js
@@ -7,6 +7,11 @@ import request from 'request-promise'
 import pictures from '../pictures.js'
 import fixtures from './fixtures/'
 
+test.beforeEach(async t => {
+  let srv = micro(pictures)
+  t.context.url = await listen(srv)
+})
+
 test('GET /:id', async t => {
   let image = fixtures.getImage()
   let srv = micro(pictures)
@@ -15,5 +20,55 @@ test('GET /:id', async t => {
   t.deepEqual(body, image)
 })
 
-test.todo('POST /')
-test.todo('POST /:id/like')
+test('POST /', async t => {
+  let image = fixtures.getImage()
+  let url = t.context.url
+
+  let options = {
+    method: 'POST',
+    uri: url,
+    json: true,
+    body: {
+      description: image.description,
+      src: image.src,
+      userId: image.userId
+    },
+    resolveWithFullResponse: true
+  }
+  let response = await request(options)
+  t.is(response.statusCode, 201)
+  t.deepEqual(response.body, image)
+})
+
+test('POST /:id/like', async t => {
+  let image = fixtures.getImage()
+  let url = t.context.url
+
+  let options = {
+    method: 'POST',
+    uri: `${url}/${image.id}/like`,
+    json: true
+  }
+
+  let body = await request(options)
+  let imageNew = JSON.parse(JSON.stringify(image))
+  imageNew.liked = true
+  imageNew.likes = 1
+
+  t.deepEqual(body, imageNew)
+})
+
+test('GET /list', async t => {
+  let images = fixtures.getImages()
+  let url = t.context.url
+
+  let options = {
+    method: 'GET',
+    uri: `${url}/list`,
+    json: true
+  }
+
+  let body = await request(options)
+
+  t.deepEqual(body, images)
+})
